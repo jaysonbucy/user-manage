79dbf90f3ecbafe543f89eeb1dfb0ed338463817
Jayson H Bucy <jayson.h.b@gmail.com>
2020-04-06 11:16:23 -0500

    jhb-new-01

--- main.go
+++ main.go
@@ -17,16 +17,16 @@ import (
 var client *mongo.Client
 
 type User struct {
-	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	dod_id string             `json:"dod_id,omitempty" bson:"dod_id,omitempty"`
-	name   string             `json:"name,omitempty" bson:"name,omitempty"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	dod_id    string             `json:"dod_id,omitempty" bson:"dod_id,omitempty"`
+	user_name string             `json:"user_name,omitempty" bson:"user_name,omitempty"`
 }
 
 func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	var user User
 	_ = json.NewDecoder(request.Body).Decode(&user)
-	collection := client.Database("milhub_user").Collection("user")
+	collection := client.Database("milhub_user").Collection("users")
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	result, _ := collection.InsertOne(ctx, user)
 	json.NewEncoder(response).Encode(result)
@@ -34,10 +34,10 @@ func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
 func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("context-type", "application/json")
 	params := mux.Vars(request)
-	id, _ := primitive.ObjectIDFromHex((params["id"]))
+	id, _ := primitive.ObjectIDFromHex(params["id"])
 	var user User
-	collection := client.Database("milhub_user").Collection("user")
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	collection := client.Database("milhub_user").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
@@ -48,9 +48,9 @@ func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
 func GetUsersEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	var users []User
-	collection := client.Database("milhub-user").Collection("user")
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	cursor, err := collection.Find(ctx, bson.D{})
+	collection := client.Database("milhub_user").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		response.Write([]byte(`{"message": "` + err.Error() + `" }`))
@@ -75,9 +75,10 @@ func main() {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, _ = mongo.Connect(ctx, clientOptions)
+	fmt.Println(client)
 	router := mux.NewRouter()
 	router.HandleFunc("/user", CreateUserEndpoint).Methods("POST")
 	router.HandleFunc("/users", GetUsersEndpoint).Methods("GET")
-	router.HandleFunc("/user{id}", GetUserEndpoint).Methods("GET")
+	router.HandleFunc("/user/{id}", GetUserEndpoint).Methods("GET")
 	http.ListenAndServe(":12345", router)
 }
